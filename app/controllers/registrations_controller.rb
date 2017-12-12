209d392a8d76f7702fb50d7b36d93bb9e9e72255
Riubbe Oliveira <rb.oliveira@hotmail.com>
2017-12-12 18:29:33 +0000

    Indluding RestratirionsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,11 @@
 class RegistrationsController < ApplicationController
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :description, :email, :password, :password_confirmation, :avatar)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :description, :email, :password, :password_confirmation, :current_password, :avatar)
+  end
 end
